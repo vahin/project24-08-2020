6d9d2811f12fd92c0ddff63116279357ec8a6b0c
Vahin Sharma <vahinsharma@gmail.com@users.noreply.github.com>
2020-08-24 18:33:16 +0530

    ­

--- index.py
+++ index.py
@@ -9,14 +9,14 @@ def x(csv, column):
     y2 = [x for x in listX if x>listXMean-(2*listXStd) and x<listXMean+(2*listXStd)]
     y3 = [x for x in listX if x>listXMean-(3*listXStd) and x<listXMean+(3*listXStd)]
     return [listX, listXMean, listXMedian, listXMode, listXStd, y1, y2, y3]
-
-print("Mean of Reading Score:", x("studentPerformance.csv", "reading score")[1])
-print("Meadian of Reading Score:", x("studentPerformance.csv", "reading score")[2])
-print("Mode of Reading Score:", x("studentPerformance.csv", "reading score")[3])
-print("Standard Deviation of Reading Score:", x("studentPerformance.csv", "reading score")[4]);print();print()
-print("Percentage of data within First Standard Deviation of Reading Score:", len(x("studentPerformance.csv", "reading score")[5])*100/len(x("studentPerformance.csv", "reading score")[0]));print()
-print("Data within First Standard Deviation of Reading Score:", x("studentPerformance.csv", "reading score")[5]);print()
-print("Percentage of data within Second Standard Deviation of Reading Score:", len(x("studentPerformance.csv", "reading score")[6])*100/len(x("studentPerformance.csv", "reading score")[0]));print()
-print("Data within Second Standard Deviation of Reading Score:", x("studentPerformance.csv", "reading score")[6]);print()
-print("Percentage of data within Third Standard Deviation of Reading Score:", len(x("studentPerformance.csv", "reading score")[7])*100/len(x("studentPerformance.csv", "reading score")[0]));print()
-print("Data within Third Standard Deviation of Reading Score:", x("studentPerformance.csv", "reading score")[7]);print()
+csv,col = "studentPerformance.csv","reading score"
+print("Mean of Reading Score:", x(csv, col)[1])
+print("Meadian of Reading Score:", x(csv, col)[2])
+print("Mode of Reading Score:", x(csv, col)[3])
+print("Standard Deviation of Reading Score:", x(csv, col)[4]);print();print()
+print("Percentage of data within First Standard Deviation of Reading Score:", len(x(csv, col)[5])*100/len(x(csv, col)[0]));print()
+print("Data within First Standard Deviation of Reading Score:", x(csv, col)[5]);print()
+print("Percentage of data within Second Standard Deviation of Reading Score:", len(x(csv, col)[6])*100/len(x(csv, col)[0]));print()
+print("Data within Second Standard Deviation of Reading Score:", x(csv, col)[6]);print()
+print("Percentage of data within Third Standard Deviation of Reading Score:", len(x(csv, col)[7])*100/len(x(csv, col)[0]));print()
+print("Data within Third Standard Deviation of Reading Score:", x(csv, col)[7]);print()
